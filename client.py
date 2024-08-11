17249698c4d589a3506b1af0ee8a0d3639d93b79
Kam4T0r <158210191+Kam4T0r@users.noreply.github.com>
2024-08-11 12:47:56 +0200

    v1.1.2
    added comments and fixed my english

--- client.py
+++ client.py
@@ -1,28 +1,37 @@
-import urllib.request
-import websockets.sync.client as ws
-import threading
-from prompt_toolkit import prompt
+import urllib.request #to make get request
+import websockets.sync.client as ws #to connect to websocket
+import threading #multithreading
+#to avoid text getting over
+from prompt_toolkit import prompt 
 from prompt_toolkit.shortcuts import print_formatted_text
 
-res = urllib.request.urlopen('https://utilsy.glitch.me/rat.txt').read()
-IP =  str(res).replace('b','').replace(r"'",'')
+res = urllib.request.urlopen('https://utilsy.glitch.me/rat.txt').read() #make get request
+IP =  str(res).replace('b','').replace(r"'",'') #get ony IP of server
 
 try:
-    with ws.connect(f'ws://{IP}:2208') as socket:
-        print('Connection established')
-        usrName = str(input('Enter your username\n>'));
+    with ws.connect(f'ws://{IP}:2208') as socket: #connect to server IP on port 2208 using websocket protocol
+        print('Connection established') #log
+        usrName = str(input('Enter your username\n>')); #ask for username
+        #avoid SERVER username so nobody trolls
         if usrName == 'SERVER':
             print('there cannot be any user named server')
             exit()
-        socket.send(f'[SERVER] User "{usrName}" joined the chat')
-        def wait():
+        socket.send(f'[SERVER] User "{usrName}" joined the chat') #send msg to server.js that usrName joined
+        def wait(): #define function that waits for messages from server
             while True:
-                print_formatted_text(f"{socket.recv()}\n",flush=True,end='')
-                print('',end='',flush=True)
-        waitT = threading.Thread(target=wait)
-        waitT.start()
-        while True:
-            msg = prompt('>')
-            socket.send(f'[{usrName}] {msg}')
+                print_formatted_text(f"{socket.recv()}\n",flush=True,end='') #print message fetched from server.js
+                print('',end='',flush=True) #flush stdout
+        waitT = threading.Thread(target=wait) #make new thread
+        waitT.start() #start new thread
+        while True: #take input 4ever from user and send it to server so it can be displayed
+            msg = prompt('>') #take input
+            socket.send(f'[{usrName}] {msg}') #send message to server.js
 except:
-    print("Couldn't establish connection")
+    print("Couldn't establish connection") #log error
+"""
+there are 2 threads, 1 with function wait so it can display messages the moment server sends it here
+
+main thread takes input and sends it to server
+
+waitT only waits for messages from server and displays it so sending and receiving works in parallel
+"""
--- server.js
+++ server.js
@@ -1,36 +1,38 @@
-const axios = require('axios');
-const WSserver = require('websocket').server;
-const http = require('http');
+const axios = require('axios'); //to make get requests
+const WSserver = require('websocket').server; //to establish tcp tunnels
+const http = require('http'); //to make server
 
 async function main()
 {
-    let res = await axios.get('https://utilsy.glitch.me/rat.txt');
-    let IP = res.data;
+    let res = await axios.get('https://utilsy.glitch.me/rat.txt'); //make get to file with IP of server
+    let IP = res.data; //get only contains of .txt without http header
+    //not important here
 
-    const server = http.createServer();
-    server.listen('2208',()=>
+    const server = http.createServer(); //create http server
+    server.listen('2208',()=> //make it run on port 2208
         {
-            console.log('listening on port 2208')
+            console.log('listening on port 2208'); //log
+            console.log('waiting for incoming connections'); //log
         });
 
-    const wsServer = new WSserver(
+    const wsServer = new WSserver( //create new websocket
         {
             httpServer: server,
         });
-    
-    wsServer.on('request',(request)=>
-        {
-            const conn = request.accept(null,request.origin);
-            console.log('Connection established');
 
-            conn.on('close',()=>
+    wsServer.on('request',(request)=> //this will run if someone connects
+        {
+            const conn = request.accept(null,request.origin); //accept request to establish tcp connection
+            console.log('Connection established'); //log
+
+            conn.on('close',()=> //log when someone disconnects
                 {
                     console.log('Connection closed');
                 });
-            conn.on('message',(data)=>
+            conn.on('message',(data)=> //when you get message
                 {
-                    console.log(data.utf8Data);
-                    wsServer.broadcast(data.utf8Data);
+                    console.log(data.utf8Data); //it displays it here in console
+                    wsServer.broadcast(data.utf8Data); //and sends it to everyone that is connected
                 });
         });
 }
